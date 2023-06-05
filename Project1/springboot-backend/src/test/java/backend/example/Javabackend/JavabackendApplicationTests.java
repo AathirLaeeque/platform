0d22b070c7daf1329bf783d0ebd9eb91b8f835bf
aathirlaeeque <aathir.laeeque@leucinetech.com>
2023-06-05 12:16:03 +0530

    Reactjava

---
+++ Project1/springboot-backend/src/test/java/backend/example/Javabackend/JavabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.example.Javabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
