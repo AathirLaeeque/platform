ba77a99a946cf18ccf23877baedbcf0703f490bf
aathirlaeeque <aathir.laeeque@leucinetech.com>
2023-06-06 09:53:58 +0530

    executable permissions given

--- backend/src/test/java/com/example/demo/DemoApplicationTests.java
+++ backend/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.Javabackend;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JavabackendApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
